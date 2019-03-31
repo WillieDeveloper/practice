10c8aed3d84bc6519b9e10341dc7d8bcfefe1a2f
WillieDeveloper <48800801+WillieDeveloper@users.noreply.github.com>
2019-03-31 13:12:13 +0300

    Добавлена проверка на пустоту полей ввода имени и возраста в ajax.js

--- script/ajax.js
+++ script/ajax.js
@@ -9,24 +9,47 @@ function add() {
 }
 */
 function sendAjaxForm() {
-    $.ajax({
-        // Имя обработчика
-        url: 'addUser',
-        //Метод отправки
-        type: 'POST',
-        //Формат данных
-        dataType: 'html',
-        data: $('#form_ajax').serialize(),
-        //Есть ответ от обработчика
-        success: function(response) {
-        	result = $.parseJSON(response);
-        	$('#result').html('Имя: '+result.name+' Возраст: '+result.age+' '+result.msg);
-        },
-        //Нет ответа от обработчика
-    	error: function(response) {
-            $('#result').html('Ответ от сервера не был получен!');
-    	}
- 	});
+    var error = 0;
+    //Проверяем поле ввода имени на пустоту           
+    if(!$('#user_name').val ()) {// если поле пустое
+        $('#user_name').css ('border', 'red 1px solid');// устанавливаем рамку красного цвета
+        error = 1;// определяем индекс ошибки
+        $('#result').html ('Введите имя!');      
+                                               
+    } else {
+        $('#user_name').css ('border', 'green 1px solid');// устанавливаем рамку зелёного цвета
+    }
+    //Проверяем поле ввода возраста на пустоту
+    if(!$('#user_age').val ()) {// если поле пустое
+        $('#user_age').css ('border', 'red 1px solid');// устанавливаем рамку красного цвета
+        error = 1;// определяем индекс ошибки
+        $('#result').html ('Введите имя!');      
+                                               
+    } else {
+        $('#user_age').css ('border', 'green 1px solid');// устанавливаем рамку зелёного цвета
+    }
+
+    //Если не было ошибок, задействуем AJAX
+    if (!error) {
+        $.ajax ({
+            // Имя обработчика
+            url: 'addUser',
+            //Метод отправки
+            type: 'POST',
+            //Формат данных
+            dataType: 'html',
+            data: $('#form_ajax').serialize (),
+            //Есть ответ от обработчика
+            success: function (response) {
+                result = $.parseJSON (response);
+                $('#result').html ('Имя: '+result.name+' Возраст: '+result.age+' '+result.msg);
+            },
+            //Нет ответа от обработчика
+            error: function (response) {
+                $('#result').html ('Ответ от сервера не был получен!');
+            }
+         });
+    }         
 }
 /*
 function ajax() {
